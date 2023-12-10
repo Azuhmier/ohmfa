ba8538779453ceae5e01150818fe483f1574dc7e
NoAnchoviesPlease <108249837+NoAnchoviesPlease@users.noreply.github.com>
2023-12-10 00:17:11 -0500

    added Pattern class outline

--- proof_of_concept/proof_of_concept.py
+++ proof_of_concept/proof_of_concept.py
@@ -11,9 +11,29 @@ path = ("./proof_of_concept/testbin.txt")
 file = open(path, 'r')
 Lines = file.readlines()
 
+class Pattern:
+    pattern: str
+    pat_type: str
+    hierarchy: int
+    keep_pat: bool
+    end_pat: str
+
+    def __init__(self, pattern: str, pat_type: str, hierarchy: int, keep_pat: bool, end_pattern: str):
+        self.pattern = pattern
+        self.pat_type = pat_type
+        self.hierarchy = hierarchy
+        self.keep_pat = keep_pat
+        self.end_pat = end_pattern
+
+patterns: list[Pattern] = [Pattern("by ", "author", 1, False, "`"), Pattern(">", "title", 2, False, "`"), Pattern("http", "url", 3, True, "`")]
+
+# this `Pattern` class, and the list of patterns, should be a good replacement
+# for the pattern dictionary that's below.
+
 dspt = [ { "pattern":"by ",  "type":"author", "hierarchy":1, "keep_pattern":0, "end_pattern":"`"},
          { "pattern":">",    "type":"title",  "hierarchy":2, "keep_pattern":0, "end_pattern":"`"},
          { "pattern":"http", "type":"url",    "hierarchy":3, "keep_pattern":1, "end_pattern":"`"} ]
+
 
 node_template = {"type": None, "value": None, "parent": None, "children": [], "hierarchy": None}
 tree = copy.deepcopy(node_template)
@@ -58,12 +78,11 @@ for line in Lines:
            parent["children"].append(copy.copy(node))
 
 
-def recurse(node, lvl):
+def recurse(node, lvl=0):
     print("    "*lvl + str(node["value"]))
-    lvl = lvl + 1
+    lvl += 1
     for child in node["children"]:
        recurse(child,lvl)
-lvl = 0
 #recurse(tree, lvl)
 
 def writeit(node) :
@@ -75,7 +94,7 @@ def writeit(node) :
         #elif node["type"] != "url" :
         elif node["type"] == "title" :
             print("")
-        print( str(node["pattern"]) + str(node["value"]) )
+        print(f'{node["pattern"]}{node["value"]}') 
     for child in node["children"] :
        writeit(child)
 
