1b4209cfb25205043b386b6e02944ccb29352fcc
NoAnchoviesPlease <108249837+NoAnchoviesPlease@users.noreply.github.com>
2023-12-09 21:55:32 -0500

    changed formatting, cleaned up some lines

--- proof_of_concept/proof_of_concept.py
+++ proof_of_concept/proof_of_concept.py
@@ -2,67 +2,66 @@
 # Mon 10/16/23 13:57:24
 # By Azuhmier
 
-import sys
-import json
-import os
+#import sys
+#import json
+#import os
 import copy
-import pprint
-path = ("/Users/Azuhmier/progs/ohmfa/proof_of_concept/testbin.txt")
+#import pprint
+path = ("./proof_of_concept/testbin.txt")
 file = open(path, 'r')
 Lines = file.readlines()
 
-dspt = [ { "pattern":"by ",  "type":"author", "hiearchy":1, "keep_pattern":0, "end_pattern":"`"},
-         { "pattern":">",    "type":"title",  "hiearchy":2, "keep_pattern":0, "end_pattern":"`"},
-         { "pattern":"http", "type":"url",    "hiearchy":3, "keep_pattern":1, "end_pattern":"`"} ]
+dspt = [ { "pattern":"by ",  "type":"author", "hierarchy":1, "keep_pattern":0, "end_pattern":"`"},
+         { "pattern":">",    "type":"title",  "hierarchy":2, "keep_pattern":0, "end_pattern":"`"},
+         { "pattern":"http", "type":"url",    "hierarchy":3, "keep_pattern":1, "end_pattern":"`"} ]
 
-node_template = {"type":None, "value":None, "parent":None, "childs":[], "hiearchy":None}
+node_template = {"type": None, "value": None, "parent": None, "children": [], "hierarchy": None}
 tree = copy.deepcopy(node_template)
 tree["type"] = "root"
 tree["value"] = "ROOT"
-tree["hiearchy"] = 0
+tree["hierarchy"] = 0
+
 node = tree
-for line in Lines :
+for line in Lines:
     match = None
-    for thing in dspt :
+    for thing in dspt:
        pattern = thing["pattern"]
        result = line.find(pattern)
-       if result == 0 :
+       if result == 0:
            start = result + len(pattern)
-           if thing["keep_pattern"] == 0 :
+           if thing["keep_pattern"] == 0:
                match = line[start:]
-           else :
-               match = line[0:]
+           else:
+               match = line
 
-           if thing["end_pattern"] is not None :
+           if thing["end_pattern"] is not None:
                end = match.find(thing["end_pattern"])
-               if end != -1 :
+               if end != -1:
                    match = match[:end]
 
            parent = None
-           while parent == None :
-               node_hiearchy  = node["hiearchy"]
-               thing_hiearchy = thing["hiearchy"]
-               if node["type"] == "root" :
+           while parent is None:
+               thing_hierarchy = thing["hierarchy"]
+               node_hierarchy = node["hierarchy"]
+               if node["type"] == "root" or node_hierarchy < thing_hierarchy:
                    parent = node
                    break
-               elif node_hiearchy < thing_hiearchy :
-                   parent = node
-                   break
-               else :
+               else:
                    node     = node["parent"]
+                   
            node             = copy.deepcopy(node_template)
            node["type"]     = thing["type"]
            node["value"]    = match.strip()
-           node["hiearchy"] = thing["hiearchy"]
+           node["hierarchy"] = thing["hierarchy"]
            node["pattern"] = thing["pattern"]
            node["parent"]   = copy.copy(parent)
-           parent["childs"].append(copy.copy(node))
+           parent["children"].append(copy.copy(node))
 
 
-def recurse(node, lvl) :
+def recurse(node, lvl):
     print("    "*lvl + str(node["value"]))
     lvl = lvl + 1
-    for child in node["childs"] :
+    for child in node["children"]:
        recurse(child,lvl)
 lvl = 0
 #recurse(tree, lvl)
@@ -77,7 +76,7 @@ def writeit(node) :
         elif node["type"] == "title" :
             print("")
         print( str(node["pattern"]) + str(node["value"]) )
-    for child in node["childs"] :
+    for child in node["children"] :
        writeit(child)
 
 writeit(tree)
