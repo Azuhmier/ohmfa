8bd1ee2630902cf7a1b6a7d5c3d023037004ddc4
azuhmier <azuhmier@gmail.com>
2025-08-21 15:45:16 -0500

    d

--- lib/ohmfa/ohmfa_url.py
+++ lib/ohmfa/ohmfa_url.py
@@ -32,25 +32,18 @@ class OhmfaUrl(Ohmfa):
     url_type   = None
     bp_type    = None
 
-
     def __init__(self, url):
         super().__init__()
         self.logger.debug(f"OhmfaUrl.__init__()")
         self.logger.debug(f"...url = {url}")
-
         self.url       = urlparse(url)
         self.pr = PathResolver2()
         self.ws = {}
         self.actions=[]
-
-        # Determine sld
         dmn_frags = self.url.netloc.split('.')
         self.sld = dmn_frags[1]
         if len(dmn_frags) == 2:
             self.sld = dmn_frags[0]
-
-        # Resolve Domain
-        #cnfgs  = [v for k,v in self.dcnfg.items() if v['sld'] == self.sld]
         cnfgs  = [v for k,v in self.dcnfg.items() if k == self.sld]
         if len(cnfgs) > 0:
             for cnfg in cnfgs:
@@ -66,27 +59,17 @@ class OhmfaUrl(Ohmfa):
         self.logger.debug(f"OhmfaUrl.resl_dmn()")
         hstn, sld, tld   = None,None,None
         dmn_type         = None
-
-        # For domains that omit the hostname when it is 'www'
         if len(dmn_frags) == 2:
             dmn_frags = [''] + dmn_frags
-
         hstn, sld, tld = dmn_frags
         for dmn_type, cnfg in dmn_cnfgs.items():
-
-            #TLD
             if tld not in cnfg['tlds']:
                 continue
-
-            # First tld in list is preffered
             new_tld  = cnfg['tlds'][0]
             self.tld = new_tld 
-
-            #HSTN
             if not cnfg['hstn_var']:
                 if hstn not in cnfg['hstns']:
                     continue
-                # First hstnm in list is preffered
                 new_hstn  = cnfg['hstns'][0]
                 self.hstn = new_hstn
             else:
@@ -95,13 +78,10 @@ class OhmfaUrl(Ohmfa):
             break
         else:
             return False
-
         new_dmn_frags   = [self.hstn, self.sld, self.tld]
         new_dmn_frags   = [x for x in new_dmn_frags if x]
         self.dmn = '.'.join(new_dmn_frags)
         return True
-
-
 
     def resl_path(self): 
         self.logger.debug(f"OhmfaUrl.resl_path()")
@@ -124,11 +104,6 @@ class OhmfaUrl(Ohmfa):
                             ar[0] = ar[0][1:]
                         else:
                             ar.pop(0)
-                #print(f"    resolving path =========")
-                #print(f"    ...node_type: {node_type}")
-                #print(f"    ...url_type: {url_type}")
-                #print(f"    ...ar: {ar}")
-                #print(f"    ...bp_cnfg: {bp}")
                 res, bp, nar = self.pr.start_recursion(bp,ar)
                 if res:
                     self.fcnfg = url_cnfg 
@@ -146,7 +121,6 @@ class OhmfaUrl(Ohmfa):
 
     def uid(self):
         a = self.cnfg["site_key"]
-        #print(f"{a}.{self.node_type}.{self.url_type}.{self.bp_type}")
 
     def update_url(self):
         self.logger.debug(f"OhmfaUrl.update_url()")
@@ -154,41 +128,24 @@ class OhmfaUrl(Ohmfa):
         self.url = self.url._replace(query=newquery)
         self.url = self.url._replace(netloc=self.dmn)
 
-
     def resl_query(self):
         self.logger.debug(f"OhmfaUrl.resl_query()")
-        #print(f"    resolving query")
         ccfg = self.cnfg['nodes'][self.node_type][self.url_type]
-        #print(f"    ...query exists?: {'query' in ccfg}")
         if 'query' in ccfg:
             query_bp  = self.cnfg['nodes'][self.node_type][self.url_type]['query']
             query     = self.url.query
             query     =  parse_qs(query)
-
-            #print(f"    ....bp_query: {query_bp}")
-            #print(f"    ....query: {query}")
-            
             new_query = {}
-
-            #print(f"    ...resolving queries: {query}")
             for k,v in query_bp.items():
-                #print(f"        ===============")
-                #print(f"        ...query ?{k}={v}")
                 vrnm, item_type, arg_ar = process_item(v)
                 if item_type not in ['bool', 'ltrl']: 
-                    #print(f"        ...bp key is '{k}'")
-                    #print(f"        ...bp vtype is '{vrnm}'")
-                    #print(f"        ...bp value is '{v}'")
                     if k not in query:
                         if len(arg_ar[0]) == 3:
-                            #print(f"        ...set value exists {arg_ar[0]['set']}")
                             new_query[k] = arg_ar[0]['set']
                 else :
                     new_query[k] = v
             self.query = new_query
-            #print(f"{'':>8}new_query: {self.query}")
             return True
         else: 
             self.query = {}
-            #print(f"...passing")
             return True
